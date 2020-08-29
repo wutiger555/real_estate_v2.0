63fd2438f847f46c797fd5affbd991b750be4a57
Max Wu <wutiger555@gmail.com>
2020-08-30 04:19:57 +0800

    0830 changes

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
-
 
 app = Flask(__name__)
 bootstrap=Bootstrap(app)
 
-
 @app.route('/')
 def home():
-    return render_template('dashboard.html')
+    return render_template('login.html')
 
 @app.route('/index')
 def index():
@@ -25,6 +23,25 @@ def user():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/login_submit', methods=['POST'])
+def login_submit():
+    if request.method == 'POST':
+        role = request.values['username']
+        if role == 'buyer':
+            return render_template('dashboard.html')
+        else:
+            return render_template('dashboard_gov.html')
+        return render_template('login.html')
+
+@app.route('/index_gov')
+def index_gov():
+    return render_template('dashboard_gov.html')
+
+@app.route('/user_gov')
+def user_gov():
+    return render_template('user_gov.html')
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\wutig\Desktop\real_estate_v2.0\env
+set VIRTUAL_ENV=C:\Users\ASUSPRO\Documents\GitHub\real_estate_v2.0\env
 
 if not defined PROMPT set PROMPT=$P$G
 
